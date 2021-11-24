9cc7829e46f64de4708e143a7dc9f0408f14419c
Riccardo Zulla <riccardozulla@gmail.com>
2021-11-24 18:19:19 +0100

    Refactor wonPoint method

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,6 +1,6 @@
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int playerOneScore = 0;
     private int playerTwoScore = 0;
     private final String playerOneName;
@@ -12,61 +12,54 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            playerOneScore += 1;
-        else
-            playerTwoScore += 1;
+        if (playerOneName.equals(playerName)) playerOneScore++;
+        else playerTwoScore++;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (playerOneScore == playerTwoScore)
-        {
-            switch (playerOneScore)
-            {
+        int tempScore = 0;
+        if (playerOneScore == playerTwoScore) {
+            switch (playerOneScore) {
                 case 0:
-                        score = "Love-All";
+                    score = "Love-All";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score = "Fifteen-All";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score = "Thirty-All";
                     break;
                 default:
-                        score = "Deuce";
+                    score = "Deuce";
                     break;
-                
+
             }
-        }
-        else if (playerOneScore >=4 || playerTwoScore >=4)
-        {
+        } else if (playerOneScore >= 4 || playerTwoScore >= 4) {
             int minusResult = playerOneScore - playerTwoScore;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = playerOneScore;
-                else { score+="-"; tempScore = playerTwoScore;}
-                switch(tempScore)
-                {
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = playerOneScore;
+                else {
+                    score += "-";
+                    tempScore = playerTwoScore;
+                }
+                switch (tempScore) {
                     case 0:
-                        score+="Love";
+                        score += "Love";
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score += "Fifteen";
                         break;
                     case 2:
-                        score+="Thirty";
+                        score += "Thirty";
                         break;
                     case 3:
-                        score+="Forty";
+                        score += "Forty";
                         break;
                 }
             }
